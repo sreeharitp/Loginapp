d82562747a29bdfdb0a4b7efb20aba3293625528
Sreehari TP <sreeharitp33@gmail.com>
2021-12-02 19:30:40 +0530

    ver3

--- loginproject/settings.py
+++ loginproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a4bm_&lqak==1gdvq1(qy4ff)6xqy116kx(o!h+jc27kfwu%g(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
